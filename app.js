09bccb4b52c62cd8f847d39bd76432b10bc5aaf2
mehmehsan <66156650+mehmehsan@users.noreply.github.com>
2020-12-06 11:22:28 +0530

    added server script

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-var btnTranslate=document.querySelector("#btntranslate");
+var btnTranslate = document.querySelector("#btntranslate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function getTranslationURL(text) {
+
+    return serverURL + "?" + "text#" + text
+
+}
+
+function errorHandler(error) {
+    console.log("error occured" , error)
+    alert("something wrong with server")
+}
+
 console.log(txtInput);
 
-btnTranslate.addEventListener("click", clickHandler);
 
 
 function clickHandler() {
-    console.log("clicked");
-    outputDiv.innerText = " ajs"+txtInput.value[0]+" " + txtInput.value +"ahh";
-} 
 
+    // outputDiv.innerText = " ajs"+txtInput.value[0]+" " + txtInput.value +"ahh";
+
+    var inputTet = txtInput.Value;
+
+    fetch(getTranslationURL(txtInput))
+        .then(response => response.json())
+        .then(json => console.log(json.contents.translated))
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler);
